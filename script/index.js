e2d1cebde1da7bff48bcfc6a27fb1b166a618cfa
Nom-an <nomanvi123@gmail.com>
2026-09-11 22:57:15 +0600

    button added via api calling

---
+++ script/index.js
@@ -0,0 +1,33 @@
+const loadLesson = ()=>{
+    const url = 'https://openapi.programming-hero.com/api/levels/all';
+
+    fetch(url)
+    .then((res)=>res.json())
+    .then((json)=>display(json.data))
+}
+
+const display = (lesson)=>{
+
+        const btnContainer = document.getElementById("btn-container");
+        btnContainer.innerHTML= "";
+
+    lesson.forEach((les)=>{
+        console.log(les.level_no);
+
+        const divBtn = document.createElement('div');
+        divBtn.innerHTML = `
+               <button class="btn btn-outline btn-primary ">
+                <span><i class="fa-solid fa-book-open"></i></span>Lesson - ${les.level_no}</button>
+        
+        `
+
+        btnContainer.append(divBtn);
+        
+       
+    
+        
+    })
+}
+
+
+loadLesson();
